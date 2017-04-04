3fc57ae39fa51fcfcd5f83813cab68f76ab68a23
Macarena Poo <macarenapoo@gmail.com>
2017-04-04 01:51:22 -0700

    Compress Files

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.css'
 const Header = () => (
   <div className={ styles.header }>
     <div className={ styles.logoWrapper }>
-      <img src="images/macarenapoo2017.png"/>
+      <img src="./images/macarenapoo2017.png"/>
     </div>
     <div className={ styles.intro }>
       <p className={ styles.h2 }>Hi! I'm Macarena, a UX Designer and Front End Developer.</p>
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin"),
   postCssSimpleVars = require("postcss-simple-vars"),
   cssVariables = require("./src/css-variables.js"),
   autoprefixer = require("autoprefixer"),
-  cssLoaderConfig = "css-loader?modules&importLoaders=1&localIdentName=[name]_[local]__[hash:base64:5]!postcss-loader";
+  cssLoaderConfig = "css-loader?modules&importLoaders=1&localIdentName=[name]_[local]__[hash:base64:5]!postcss-loader",
+  webpack = require("webpack"),
+  CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
@@ -12,8 +14,7 @@ module.exports = {
   },
   devServer: {
     inline: true,
-    port: 3333,
-    contentBase: 'public'
+    port: 3333
   },
   module: {
     loaders: [
@@ -51,6 +52,26 @@ module.exports = {
     ];
   },
   plugins: [
-    new ExtractTextPlugin("bundle.css")
+    new ExtractTextPlugin("bundle.css"),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ]
 };
